178031b3dcaf25bc6138ca78f5a1e69f4ca3c391
Sanya Midha <sanyamidha.10@gmail.com>
2023-05-21 23:01:49 +0530

    Media files configuration

--- employees/models.py
+++ employees/models.py
@@ -11,3 +11,5 @@ class Employee(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.first_name
--- mysite/settings.py
+++ mysite/settings.py
@@ -122,7 +122,11 @@ STATICFILES_DIRS = [
     'mysite/static'
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
